049ac03d8afcc9c4728a6756a211932ac194e348
Prasanna-Ganesh <120724118+Prasanna-Ganesh@users.noreply.github.com>
2023-03-29 15:41:27 +0530

    Update question5.py

--- Project/Prasanna_Project/question5.py
+++ Project/Prasanna_Project/question5.py
@@ -3,8 +3,3 @@ import csv
 csvFile = csv.reader(open('https://reqres.in/api/users?page=1'),"rb")
 for i in csvFile:
     print(i)
-# import pandas as pd
-# import requests
-# url='https://reqres.in/api/users'
-# s=requests.get(url).content
-# c=pd.read_csv(s)
